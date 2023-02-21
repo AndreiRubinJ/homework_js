7b0bf8b045be4c2f83df06218032f925ef8c8e24
arubin <andrew.a.rubin@hotmail.com>
2023-02-21 21:25:21 +0200

    hw15

--- JS_15/gulpfile.js
+++ JS_15/gulpfile.js
@@ -8,45 +8,67 @@ const injectPartials = require('gulp-inject-partials');
 const imagemin = require("gulp-imagemin");
 
 
+const sorces = {
+    base_path: "src",
+    component: "components",    
+}
+
+const distanation = {
+    base_path: "app",
+    templates: "temp",
+    style: "style",
+    images: "img",
+    js: "scripts"
+}
+
+const component = `${sorces.base_path}/${sorces.component}`;
+const app_temp = `${distanation.base_path}/${distanation.templates}`;
+const app_js = `${distanation.base_path}/${distanation.js}`;
+const app_style = `${distanation.base_path}/${distanation.style}`;
+const app_img = `${distanation.base_path}/${distanation.images}`;
+
+
+
+
 task('styles', () => {     
-        return src(['src/scss/main.scss', 'src/components/**/*.scss'])
+        return src([`${sorces.base_path}/scss/main.scss`, `${component}/**/*.scss`])
             .pipe(sass().on('error', sass.logError))
-            .pipe(dest('app/temp/'));
+            .pipe(dest(`${app_temp}/`));
 });
 
 task('styles_combaine', () => {
-    return src(['app/temp/**/*.css'])
+    return src([`${app_temp}/**/*.css`])
          .pipe(combine())
          .pipe(concat('style.css'))
-         .pipe(dest('app/style/'));
+         .pipe(dest(`${app_style}/`));
 });
 
 task('import_js', function() {
-    return src(['src/components/**/*.js','src/main.js'])
+    return src([`${component}/**/*.js`,`${sorces.base_path}/main.js`])
           .pipe(concat('scripts.js'))
-          .pipe(dest('app/scripts'));
+          .pipe(dest(`${app_js}`));
   });
 
  task('html', function () {
-    return src('src/index.html')
+    return src(`${sorces.base_path}/index.html`)
              .pipe(injectPartials())
-             .pipe(dest('app'));
+             .pipe(dest(`${distanation.base_path}`));
   });
 
 task('cleare', () => {
-    return src("app/*", { allowEmpty: true })
+    return src(`${distanation.base_path}/*`, { allowEmpty: true })
         .pipe(clean());
 });
 
 task('cleare_temp', () => {
-    return src("app/temp", { allowEmpty: true })
+    return src(`${app_temp}`, { allowEmpty: true })
         .pipe(clean());
 });
 
 task('images', () => {
-    return src("src/components/**/img/*")
+    return src(`${component}/**/img/*`)
     .pipe(imagemin())
-    .pipe(dest("app/img/"));
+    .pipe(dest(`${app_img}/`));
 });
 
 task('set_styles', series(['styles', 'styles_combaine']));
